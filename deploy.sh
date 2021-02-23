d267d12d22a267fd79204abc57a850499d50f060
Qubo Qin <qubo.qin.2018@gmail.com>
2021-02-23 11:22:53 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/homepage.git master:gh-pages
+git push -f git@github.com:quboqin/homepage.git master:gh-pages
 
 cd -
